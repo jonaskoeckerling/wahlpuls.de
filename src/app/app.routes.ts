fe7ee1b6c940093fc2bbd79a71efe1dafc9b246e
Jonas Koeckerling <j.koeckerling@me.com>
2025-03-18 16:03:20 +0800

    Add imprint content and removed privacy site because it is not needed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ImprintComponent } from './pages/imprint/imprint.component';
-import { PrivacyComponent } from './pages/privacy/privacy.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'impressum', component: ImprintComponent },
-    { path: 'datenschutz', component: PrivacyComponent },
 ];
